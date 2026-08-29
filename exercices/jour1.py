8c0db730543a966513050ea8ded8275ad236f570
Yassine <yassinechk98@gmail.com>
2026-08-29 18:11:05 +0100

    Fonction python pour print mon nom

--- exercices/jour1.py
+++ exercices/jour1.py
@@ -1,5 +1,10 @@
-prenom = "Yassine"
-age = 27
-ville = "monastir"
-apprend_python = True
-print(f"Je m'appelle {prenom}, j'ai {age} ans, j'habite à {ville}.")
+def my_name():
+    prenom = "Yassine"
+    age = 27
+    ville = "monastir"
+    apprend_python = True
+    print(f"Je m'appelle {prenom}, j'ai {age} ans, j'habite à {ville}.")
+    print(f"J'apprends à coder : {apprend_python}")
+
+
+
